d74e9c22250bde449018e598cef592fac4b4b951
Marliana Lara <marliana.lara@gmail.com>
2016-11-02 17:19:02 -0400

    install local storage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 // Vendored assets
 //= require angular/angular.js
-//= require ng-local-storage/dist/ng-local-storage.js
+//= require angular-local-storage/dist/angular-local-storage.js
 //= require angular-ui-router/release/angular-ui-router.js
 
 // Application assets
