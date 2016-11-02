4b74f5bdfd054b294541f95267f0078e69ab3f72
Marliana Lara <marliana.lara@gmail.com>
2016-11-02 14:30:04 -0400

    add angular-ui-router

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // Vendored assets
 //= require angular/angular.js
 //= require ng-local-storage/dist/ng-local-storage.js
-
+//= require angular-ui-router/release/angular-ui-router.js
 
 // Application assets
 //= require app.module.js
