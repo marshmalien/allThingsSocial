9740401c17886dc6b70f96da5264e38e445cfc0e
Marliana Lara <marliana.lara@gmail.com>
2016-11-02 22:31:20 -0400

    styled top navigation bar

--- app/assets/javascripts/app.module.js
+++ app/assets/javascripts/app.module.js
@@ -3,13 +3,12 @@
 
   angular.module('app', ['ui.router', 'LocalStorageModule', 'templates'])
     .config(function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/');
 
       $stateProvider.state('home', {
         url: '/',
         templateUrl: 'home.html'
       }).state('profile', {
-        template: "<p>profile page</p>"
-      })
+        templateUrl: 'profile.html'
+      });
     });
 })();
