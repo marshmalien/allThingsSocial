a8e691443ee9ffe29d90979e733d7cadd5595676
Marliana Lara <marliana.lara@gmail.com>
2016-11-03 21:57:06 -0400

    add routing to quiz 2

--- app/assets/javascripts/app.module.js
+++ app/assets/javascripts/app.module.js
@@ -12,14 +12,18 @@
         url: 'home',
         templateUrl: 'home.html',
         controller: 'homepageController as homepage'
-      }).state('parent.profile', {
-        url: 'profile',
-        templateUrl: 'profile.html',
-        controller: 'childpageController as childpage'
       }).state('parent.quizOne', {
         url: 'quiz-one',
         templateUrl: 'quiz-one.html',
         controller: 'quizOneController as quizOne'
+      }).state('parent.profile', {
+        url: 'profile',
+        templateUrl: 'profile.html',
+        controller: 'childpageController as childpage'
+      }).state('parent.quizTwo', {
+        url: 'quiz-two',
+        templateUrl: 'quiz-two.html',
+        controller: 'quizTwoController as quizTwo'
       });
     })
 })();
