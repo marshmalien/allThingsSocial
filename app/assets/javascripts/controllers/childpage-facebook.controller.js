8791e8cb5e3601c8854ad703f7117ec55bfc7598
Marliana Lara <marliana.lara@gmail.com>
2016-11-03 20:00:28 -0400

    adjust position of popup arrows

--- app/assets/javascripts/controllers/childpage-facebook.controller.js
+++ app/assets/javascripts/controllers/childpage-facebook.controller.js
@@ -1,8 +1,8 @@
 angular.module('app').controller('childpageController', function() {
   this.instructionSteps = [
-    {name: 'stepOne', arrow: 'left', description: "This is where you can write message for all of your daughter's friends to see. Please keep the short and sweet!"},
+    {name: 'stepOne', arrow: 'left', description: "This is where you can write message for all of your daughter's friends to see. Please keep them short and sweet!"},
     {name: 'stepTwo', arrow: 'left', description: 'This is where you will send 99% of your messages to your daughter.'},
-    {name: 'stepThree', arrow: 'right', description: "You probably don't need to suggest friends for your daughter."},
+    {name: 'stepThree', arrow: 'down', description: "You probably don't need to suggest friends for your daughter."},
     {name: 'stepComplete', arrow: null, description: 'You made it through the tutorial. Are you ready for the quiz?'}
   ]
 
