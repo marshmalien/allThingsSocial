07485816bbc610c620248ebd53df73aa743fefe0
Marliana Lara <marliana.lara@gmail.com>
2016-11-05 14:19:32 -0400

    change response text color based on answer

--- app/assets/javascripts/controllers/question.controller.js
+++ app/assets/javascripts/controllers/question.controller.js
@@ -5,13 +5,17 @@
     this.currentQuizQuestions = $scope.questions[$stateParams.id];
     this.question = this.currentQuizQuestions[$stateParams.questionId];
     this.answered = false;
-    this.answerResponse = "Bummer!";
+    this.isCorrect = false;
+    this.answerResponse = "";
 
     this.nextQuestion = function(key) {
       this.answered = true;
       $scope.quiz.answerArray.push({userInput: key, answer: this.question.answer});
       if (this.question.answer === key) {
         this.answerResponse = "Correct!";
+        this.isCorrect = true;
+      } else {
+        this.answerResponse = "Bummer!";
       }
       if (Number($stateParams.questionId) < this.currentQuizQuestions.length - 1) {
         $timeout(function () {
